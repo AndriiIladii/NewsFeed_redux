6c3ffb7b7783943f5d5baaeabe9f3ee78110ec9f
AndriiIladii <iladiy21@gmail.com>
2024-05-19 20:31:44 +0300

    commit

--- src/components/NewsPost.jsx
+++ src/components/NewsPost.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "./NewsPost.module.css";
 
 const NewsPost = ({ news }) => {
+  const [likes, setLikes] = useState(0);
+
+  function handleLikes() {
+    setLikes(likes + 1);
+  }
+
   return (
     <div>
       <ul className={styles.newsFeed}>
         {news.map((item) => (
           <li className={styles.feedItem} key={item.id}>
             <p>{item.value}</p>
-            <div>
-              <button>Like</button>
+            <div className={styles.postBtns}>
+              <button className={styles.likeBtn} onClick={handleLikes}>
+                <span>{likes}</span>
+                Like
+              </button>
               <button>Comment</button>
               <button>Share</button>
             </div>
