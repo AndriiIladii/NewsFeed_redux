fb94751c1547cc481017c1723aa0056a23b723fd
AndriiIladii <iladiy21@gmail.com>
2024-05-24 12:53:29 +0300

    useEffect func

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewsPost from "./NewsPost";
 
 const NewsFeed = ({ news, setNews }) => {
+  useEffect(() => {
+    console.log(location.hash.substring(1));
+  });
+
   function handleLikes(id) {
     let updateLikes = news.map((item) =>
       item.id === id ? { ...item, likes: item.likes + 1 } : item
